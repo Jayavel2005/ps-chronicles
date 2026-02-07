97e0d9a3aaf940327b1ac1258e1ea1fcaeb74e76
Jayavel <jayavel1520@gmail.com>
2026-02-07 11:56:03 +0530

    feat: final keyword and garbage collection

--- src/OOPs/OOP1.java
+++ src/OOPs/OOP1.java
@@ -1,7 +1,25 @@
 package OOPs;
+class Student{
+    int rno;
+    String name;
+    float marks;
 
+    Student(){
+        System.out.println("Hello ");
+    }
+
+    Student(int rno, String name, float marks){
+        this.rno = rno;
+        this.name = name;
+        this.marks = marks;
+
+        System.out.println("Name: " + this.name + "\nRoll No: " + this.rno + "\nMarks: " + this.marks);
+    }
+}
 public class OOP1 {
     public static void main(String[] args) {
-        
+        Student stu1 = new Student(23108026, "Jayavel", 99.2f);
+
+
     }
 }
