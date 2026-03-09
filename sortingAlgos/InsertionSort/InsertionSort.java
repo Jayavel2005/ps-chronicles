fb4fb46225f27e67f62df8c8fd90f01842c84c5a
Jayavel2005 <jayavel1520@gmail.com>
2026-03-09 22:42:19 +0530

    feat: insertion sort

---
+++ sortingAlgos/InsertionSort/InsertionSort.java
@@ -0,0 +1,35 @@
+// package sortingAlgos.InsertionSort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+
+        int[] arr = new int[n];
+
+        for(int i = 0;i<n;i++){
+            arr[i] = in.nextInt();
+        }
+
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+        
+    }
+
+    static void insertionSort(int[] arr){
+        for(int i = 1;i<arr.length;i++){
+            int key = arr[i];
+            int j = i - 1;
+            while (j>=0 && arr[j] > key) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+    }
+}
